21c5823b772b62ad096aed3eef12a75fb5199eb7
ZSPWrzesnia <56863071+ZSPWrzesnia@users.noreply.github.com>
2022-03-07 11:47:12 +0100

    Create suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,poczatek,koniec,suma;
+	cout<<"Podaj poczatek zakresu";
+	cin>>poczatek;
+	cout<<endl;
+	cout<<"Podaj koniec zakresu";
+	cin>>koniec;
+	cout<<endl;
+	suma=0;
+	x=poczatek;
+	for(x;x<=koniec;x++){
+	if (x%5==0){
+		cout<<x;
+		cout<<endl;
+		suma=suma+x;  // suma+=x
+		//x++;
+	}
+	//else x++;
+	}
+	cout<<"Suma: "<<suma;
+}
